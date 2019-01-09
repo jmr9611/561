51f0f0ec12b98f05c5797feea140812f8280a0d9
Juliet Oh <julietoh@Juliets-MacBook-Pro.local>
2019-01-09 04:14:25 -0800

    tshark command for initial RTT

--- firstscripts.py
+++ firstscripts.py
@@ -2,3 +2,6 @@
 
 # returns all DNS query responses with "Answers" -- includes IP Addresses from DNS payload
 tshark -nr netflix.pcapng -Y "dns" -V 
+
+# returns initial RTT (delay between SYN and SYN-ACK)
+tshark -r test1.pcap -Y 'ip.addr == 10.0.0.230' -T fields -e tcp.analysis.initial_rtt
