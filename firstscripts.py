f8794a2417f9685c0a9ff46a3c119efdf5558bbb
Jose Rodriguez <jmr9611@gmail.com>
2019-01-11 15:24:35 -0500

    add testing to initial script, add test pcap files, print shell output

--- firstscripts.py
+++ firstscripts.py
@@ -1,7 +1,27 @@
 # my first file
+import subprocess
 
+# string -> string
 # returns all DNS query responses with "Answers" -- includes IP Addresses from DNS payload
-tshark -nr netflix.pcapng -Y "dns" -V 
+# tshark -nr $pcapFileName -Y "dns" -V
+def getResponses(pcapFileName):
+    return subprocess.check_output(
+    "tshark -nr " + pcapFileName + " -Y \"dns\" -V",
+    shell=True)
 
+# string, string -> string
 # returns initial RTT (delay between SYN and SYN-ACK)
-tshark -r test1.pcap -Y 'ip.addr == 10.0.0.230' -T fields -e tcp.analysis.initial_rtt
+# tshark -r $pcapFileName -Y 'ip.addr == $ipName' -T fields -e tcp.analysis.initial_rtt
+def getRTT(pcapFileName, ipName):
+    return subprocess.check_output(
+    "tshark -r " + pcapFileName + " -Y \'ip.addr == " + ipName + "\' -T fields -e tcp.analysis.initial_rtt",
+    shell=True)
+
+def test():
+    netflixPcapFileName = "netflix.pcapng"
+    testPcapFileName = "testpcap.pcapng"
+    testIP = "10.8.106.202"
+    print(getResponses(netflixPcapFileName))
+    print(getRTT(testPcapFileName, testIP))
+
+test()
